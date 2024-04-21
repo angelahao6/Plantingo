59a8e7805ee8ac6ee50abc96252992ecfbb57c70
Nicholas Wang <nicholaswang03@gmail.com>
2024-04-21 03:53:25 -0700

    add text

--- src/pages/Scanner.jsx
+++ src/pages/Scanner.jsx
@@ -1,8 +1,8 @@
-import {useRef, useState, useCallback} from 'react';
+import { useRef, useState, useCallback } from "react";
 import Webcam from "react-webcam";
-import Overlay from '../components/Overlay';
+import Overlay from "../components/Overlay";
 
-const Scanner = ({user}) => {
+const Scanner = ({ user }) => {
   const webcamRef = useRef(null);
   const [imgSrc, setImgSrc] = useState(null);
 
@@ -18,19 +18,46 @@ const Scanner = ({user}) => {
 
   return (
     <div className="container">
-      <p style={{position: "absolute", top: "5vh", left: "50%", transform: "translateX(-50%)", width: "75vw", fontSize: "20px", color: "white", backdropFilter: "blur(10px)", padding: "10px", borderRadius: "20px"}}>Press any key to take a picture!</p>
-        {imgSrc ? (
-            <div style={{ height: "100vh", width: "100%" }}>
-              <img src={imgSrc} alt="webcam" style={{ width: "100%", height: "100%", objectFit: "cover" }} />
-              <Overlay style={{position: "fixed"}} retake={retake} image={imgSrc} />
-            </div>
-        ) : (
-          <div style={{ height: "95vh", width: "100%" }}>
-            <Webcam style={{ width: "100%", height: "100%", objectFit: "cover" }} ref={webcamRef} screenshotFormat="image/jpeg"
-              onClick={capture}
-            />
-          </div>
-        )}
+      <p
+        style={{
+          position: "absolute",
+          top: "5vh",
+          left: "50%",
+          transform: "translateX(-50%)",
+          width: "75vw",
+          fontSize: "20px",
+          color: "white",
+          backdropFilter: "blur(10px)",
+          padding: "10px",
+          borderRadius: "20px",
+          textAlign: "center"
+        }}
+      >
+        Tap anywhere to take a picture!
+      </p>
+      {imgSrc ? (
+        <div style={{ height: "100vh", width: "100%" }}>
+          <img
+            src={imgSrc}
+            alt="webcam"
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+          />
+          <Overlay
+            style={{ position: "fixed" }}
+            retake={retake}
+            image={imgSrc}
+          />
+        </div>
+      ) : (
+        <div style={{ height: "95vh", width: "100%" }}>
+          <Webcam
+            style={{ width: "100%", height: "100%", objectFit: "cover" }}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+            onClick={capture}
+          />
+        </div>
+      )}
       {/* <div className="btn-container">
         {imgSrc ? (
           <></>
