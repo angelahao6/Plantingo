ee190ded233e8868e42afc220cd6a7ad7670d0b5
Ryan Yeo <ryandc.yeo@gmail.com>
2024-04-20 19:50:39 -0700

    send camera image to backend

--- src/gemini.jsx
+++ src/gemini.jsx
@@ -1,18 +1,23 @@
 
 import { useState } from 'react';
 import axios from 'axios'; // Import Axios for making HTTP requests
+import PropTypes from 'prop-types';
+import {useLocation} from 'react-router-dom';
 
-function Gemini() {
+function Gemini({ image }) {
   const [inputText, setInputText] = useState('');
   const [outputText, setOutputText] = useState('');
+
+  const location = useLocation();
 
   const handleChange = (event) => {
     setInputText(event.target.value);
   };
 
   const handleSubmit = async () => {
+    // console.log('image', location.state.image)
     try {
-      const response = await axios.post('http://127.0.0.1:5000/user_input', { message: inputText });
+      const response = await axios.post('http://127.0.0.1:5000/plant_stats', { image: location.state.image });
       setOutputText(response.data.response);
     } catch (error) {
       console.error('Error:', error.response.data);
@@ -29,3 +34,7 @@ function Gemini() {
 }
 
 export default Gemini;
+
+Gemini.propTypes = {
+  image: PropTypes.string.isRequired 
+}
